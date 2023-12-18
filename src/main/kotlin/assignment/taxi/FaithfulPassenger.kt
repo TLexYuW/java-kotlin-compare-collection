419fc96b0ff81f9f7743bb3fd2fe9f4134e9a291
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-12-18 22:38:36 +0800

    update  refactor example

---
+++ src/main/kotlin/assignment/taxi/FaithfulPassenger.kt
@@ -0,0 +1,38 @@
+package assignment.taxi
+
+/**
+ * @author : Lex Yu
+ */
+
+fun TaxiPark.findFaithfulPassengers1(minTrips: Int): Set<Passenger> =
+    trips
+        .flatMap(Trip::passengers)
+        .groupBy { passenger -> passenger }
+        .filterValues { group -> group.size >= minTrips }
+        .keys
+/*
+trips
+    .flatMap { it.passengers }
+    .groupBy { it }
+    .filter { it.value.size >= minTrips }
+    .map { it.key }
+    .toSet()
+ */
+
+
+fun TaxiPark.findFaithfulPassengers2(minTrips: Int): Set<Passenger> =
+    allPassenger
+        .filter { p ->
+            trips.count { p in it.passengers } >= minTrips
+        }
+        .toSet()
+/*
+allPassenger
+    .partition { p ->
+        trips.sumBy { t ->
+            if (p in t.passengers) 1 else 0
+        } >= minTrips
+    }
+    .first
+    .toSet()
+ */
---
+++ src/main/kotlin/assignment/taxi/TaxiPark.kt
@@ -0,0 +1,23 @@
+package assignment.taxi
+
+/**
+ * @author : Lex Yu
+ */
+data class TaxiPark(
+    val allDriver: Set<Driver>,
+    val allPassenger: Set<Passenger>,
+    val trips: List<Trip>
+)
+
+data class Driver(val name: String)
+data class Passenger(val name: String)
+
+data class Trip(
+    val driver: Driver,
+    val passengers: Set<Passenger>,
+    val duration: Int,
+    val distance: Double,
+    val discount: Double? = null
+) {
+    val cost: Double get() = (1 - (discount ?: 0.0)) * (duration + distance)
+}
